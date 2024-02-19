cf720e349009ce5e136ac89f3266a4e31d20039e
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-02-19 14:19:55 +0300

    [20240219/0] Parse commits separately

--- 20240219/0/prog.py
+++ 20240219/0/prog.py
@@ -4,4 +4,13 @@ import zlib
 
 for p in glob.iglob(sys.argv[1] + "/??/*"):
     with open(p, "rb") as f:
-        print('-' * 16, zlib.decompress(f.read()), sep='\n')
+        obj = zlib.decompress(f.read())
+    _typesize, _, obj = obj.partition(b'\x00')
+    _type, size = _typesize.split()
+    match _type:
+        case b"commit":
+            print("--- C O M M I T ---")
+            print(obj.decode().rstrip())
+        case _:
+            print("--- <other> ---")
+            print(obj)
