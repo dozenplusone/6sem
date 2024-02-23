af3635009740b6173695bb299658f162fa364e04
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-02-23 19:29:52 +0300

    [20240219/1] Show last commit in branch if its name is provided

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -1,6 +1,7 @@
 import glob
 import os
 import sys
+import zlib
 
 
 class RepoError(Exception):
@@ -17,3 +18,9 @@ if len(sys.argv) == 2:
 
     print(*glob.iglob("**", root_dir=".git/refs/heads"))
     sys.exit(0)
+
+with open(f".git/refs/heads/{sys.argv[2]}") as f:
+    sha = f.read(40)
+with open(f".git/objects/{sha[:2]}/{sha[2:]}", "rb") as f:
+    commit = zlib.decompress(f.read()).partition(b'\x00')[2].decode().rstrip()
+print(commit)
