b7f647002b1c0fbcd5b545b987309bf6e3f457e2
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-02-24 11:13:19 +0300

    [20240219/1] Show tree associated with last commit

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -8,6 +8,28 @@ class RepoError(Exception):
     pass
 
 
+def getTreeId(commitId: str):
+    with open(f".git/objects/{commitId[:2]}/{commitId[2:]}", "rb") as f:
+        commit = zlib.decompress(f.read()).partition(b'\x00')[2]
+    tree, _, commit = commit.partition(b'\n')
+    treeId = tree.partition(b' ')[2].decode()
+    return treeId
+
+
+def printTree(treeId):
+    with open(f".git/objects/{treeId[:2]}/{treeId[2:]}", "rb") as f:
+        tree = zlib.decompress(f.read()).partition(b'\x00')[2]
+    attrXname, _, idXtree = tree.partition(b'\x00')
+    attr, _, name = attrXname.partition(b' ')
+    id, tree = idXtree[:20], idXtree[20:]
+    print(attr.decode(), name.decode(), id.hex())
+    while tree:
+        attrXname, _, idXtree = tree.partition(b'\x00')
+        attr, _, name = attrXname.partition(b' ')
+        id, tree = idXtree[:20], idXtree[20:]
+        print(attr.decode(), name.decode(), id.hex())
+
+
 if len(sys.argv) == 1:
     raise RepoError("repo not provided")
 
@@ -25,6 +47,9 @@ if sys.argv[2] not in branches:
 
 with open(f".git/refs/heads/{sys.argv[2]}") as f:
     sha = f.read(40)
+
 with open(f".git/objects/{sha[:2]}/{sha[2:]}", "rb") as f:
     commit = zlib.decompress(f.read()).partition(b'\x00')[2].decode().rstrip()
 print(commit)
+
+printTree(getTreeId(sha))
