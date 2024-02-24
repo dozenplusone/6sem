b2632874a94e36c98d490bfcf3c7217a5c36efd6
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-02-24 18:33:17 +0300

    [20240219/1] Remove last commit display

--- 20240219/1/prog.py
+++ 20240219/1/prog.py
@@ -61,10 +61,6 @@ if sys.argv[2] not in branches:
 with open(f".git/refs/heads/{sys.argv[2]}") as f:
     sha = f.read(40)
 
-with open(f".git/objects/{sha[:2]}/{sha[2:]}", "rb") as f:
-    commit = zlib.decompress(f.read()).partition(b'\x00')[2].decode().rstrip()
-print(commit)
-
 print("TREE for commit", sha)
 tree, sha = getTreeParentId(sha)
 printTree(tree)
