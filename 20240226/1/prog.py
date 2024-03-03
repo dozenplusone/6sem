f3232f3ab337d9ba90ee0441bbe08ae2a8a9f988
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-03 12:31:23 +0300

    [20240226/1] Add invalid arguments warning

--- 20240226/1/prog.py
+++ 20240226/1/prog.py
@@ -7,7 +7,10 @@ monsters = {}
 
 while cmd := sys.stdin.readline().split():
     match cmd:
-        case ["up" | "down" | "left" | "right"]:
+        case ["up" | "down" | "left" | "right", *args]:
+            if len(args) > 0:
+                print("Invalid arguments")
+                continue
             if cmd[0] == "up":
                 y = y - 1 if y > 0 else 9
             elif cmd[0] == "down":
@@ -17,8 +20,14 @@ while cmd := sys.stdin.readline().split():
             elif cmd[0] == "right":
                 x = x + 1 if x < 9 else 0
             print("Moved to", (x, y))
-        case ["addmon", x, y, hello]:
-            p = int(x), int(y)
+        case ["addmon", *args]:
+            try:
+                _x, _y, hello = int(args[0]), int(args[1]), args[2]
+                assert 0 <= _x <= 9 and 0 <= _y <= 9
+            except Exception:
+                print("Invalid arguments")
+                continue
+            p = _x, _y
             flag = p in monsters
             monsters[p] = hello
             print("Added monster to", p, "saying", hello)
