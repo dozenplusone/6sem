d48b150e29fd13743861e30492bd98fb927e22b1
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-08 09:52:21 +0300

    [20240304/1] Add field hp to class Monster

--- 20240304/1/prog.py
+++ 20240304/1/prog.py
@@ -24,10 +24,10 @@ p1 = Player()
 
 
 class Monster:
-    def __init__(self, name: str, text: str):
+    def __init__(self, name: str, text: str, hp: int):
         assert name in cowsay.list_cows() or name == "jgsbat", \
                 "Cannot add unknown monster"
-        self.name, self.text = name, text
+        self.name, self.text, self.hp = name, text, hp
 
 
 monsters = {}
@@ -61,7 +61,7 @@ def runCmd(cmd: str):
             name, hello = args[0], args[3]
             p = _x, _y
             flag = p in monsters
-            monsters[p] = Monster(name, hello)
+            monsters[p] = Monster(name, hello, 0)
             print("Added monster", name, "to", p, "saying", hello)
             if flag:
                 print("Replaced the old monster")
