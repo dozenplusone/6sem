c371417cfb9d5e3e6724d684739fd9d70030d27c
Valery Zabrovsky <valthebrewer@yandex.ru>
2024-03-24 15:33:59 +0300

    Split prog.py into client.py & server.py (not working yet)

--- 20240318/1/client.py
+++ 20240318/1/client.py
@@ -4,43 +4,6 @@ import shlex
 from custom import custom
 
 
-class Player:
-    def __init__(self, x: int = 0, y: int = 0):
-        assert 0 <= x <= 9 and 0 <= y <= 9, "Invalid initial position"
-        self.x, self.y = x, y
-
-    def move(self, dx, dy):
-        self.x = (self.x + dx) % 10
-        self.y = (self.y + dy) % 10
-        encounter(self.x, self.y)
-
-    def attack(self, name, damage):
-        if ((self.x, self.y) not in monsters
-                or monsters[self.x, self.y].name != name):
-            print(f"No {name} here")
-            return
-        damage = min(monsters[self.x, self.y].hp, damage)
-        monsters[self.x, self.y].hp -= damage
-        print(f"Attacked {monsters[self.x, self.y].name}, damage {damage} hp")
-        if monsters[self.x, self.y].hp > 0:
-            print(f"{monsters[self.x, self.y].name} now has",
-                  monsters[self.x, self.y].hp)
-        else:
-            print(monsters[self.x, self.y].name, "died")
-            del monsters[self.x, self.y]
-
-
-p1 = Player()
-
-
-class Monster:
-    def __init__(self, name: str, text: str, hp: int):
-        self.name, self.text, self.hp = name, text, hp
-
-
-monsters = {}
-
-
 def encounter(x, y):
     if (x, y) not in monsters:
         return
